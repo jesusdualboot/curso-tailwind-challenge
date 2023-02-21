f9fcc69866f3612b814ff8ce3cdd41a77b7d1b47
Jesus Sanchez Esaa <jesus.sanchez@dualbootpartners.com>
2023-02-21 09:12:40 -0400

    feat: Challenge solution

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // You can use this function to open an close the navbar menu on small screens when the menu is collapsed
-function toggleNavbar(collapseID) {
-  document.getElementById(collapseID).classList.toggle('hidden');
-  document.getElementById(collapseID).classList.toggle('block');
+function toggleNavbar() {
+  document.getElementById('collapseID').classList.toggle('hidden');
+  document.getElementById('collapseID').classList.toggle('flex');
 }
